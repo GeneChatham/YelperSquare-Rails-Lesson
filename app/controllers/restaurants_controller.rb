a70911993f36787fc28eb7c9524d6968ded6d654
GeneChatham <genechatham@gmail.com>
2013-12-04 17:29:39 -0600

    Added _form for adding and updating a restaurant.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,23 +5,39 @@ class RestaurantsController < ApplicationController
 	end
 
 	def show
-
+		@restaurant = Restaurant.find(params[:id])
 	end
 
 	def new
+		@restaurant = Restaurant.new
 
 	end
 
 	def create
+		restaurant = Restaurant.new(restaurant_params)
+		restaurant.save
+		redirect_to root_path
+	end
 
+	def edit
+		@restaurant = Restaurant.find(params[:id])
 	end
 
 	def update
-
+		@restaurant = Restaurant.find(params[:id])
+		@restaurant.update(restaurant_params)
+		redirect_to root_path
 	end
 
 	def destroy
+		restaurant = Restaurant.find(params[:id])
+		restaurant.destroy
+		redirect_to root_path
+	end
 
+	private
+	def restaurant_params
+	  params.require(:restaurant).permit(:name, :hours, :cuisine, :address, :photo_url, :average_rating)
 	end
 
 end
