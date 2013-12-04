ec2f59d560dfdee001181d4e0e3e67b7c42d74cf
GeneChatham <genechatham@gmail.com>
2013-12-04 15:42:23 -0600

    Initial commit. Hirb gem added to new project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yelpersquare::Application.config.secret_key_base = '6c00bdc6027bd58bc64422cf0e55b547d85b74204861228f91e914a6743fceb5d00dead8e203b39bed72529a0fe6d012513d9f34657e03b683e28e54d1f9c4ab'
